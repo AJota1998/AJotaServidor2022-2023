f9925e836ec9154aa70e5617bbc2fdc8863207cf
ajota1998 <alum.aortizg.2021@iesalixar.org>
2022-10-18 12:50:42 +0200

    cookies

--- formularios/js/cookies1.js
+++ formularios/js/cookies1.js
@@ -1,18 +1,34 @@
+//comprobacion que el navegador tiene activas las cookies
+
+if (navigator.cookieEnabled == true) {
+    alert("Las cookies están activas");
+  } else {
+    alert("Las cookies están desactivadas");
+  }
+
 
 //creación de la cookie 
 
 function setCookie() {
-    let cname = prompt("Introduzca el nombre de la cookie");
-    let cvalue = prompt("Introduzca el valor de la cookie");
-    
-    document.cookie = cname + "=" + cvalue + ";";
+
+let n = prompt("Introduzca nombre de cookie")
+let v = prompt("Introduzca valor de la cookie")
+let exd = parseInt(prompt("Introduzca el número de dias hasta que esta expire"))
+let d = new Date();
+
+d.setTime(d.getTime() + exd * 24 * 60 * 60 * 1000);
+let exp = "expires=" + d.toUTCString(); 
+document.cookie = n + "=" + v + ";" + exp;
+
 }
+  
 
-//Leer el valor de la cookie
+//Leer el value de la cookie
 
-function getCookie(cname) {
+function getCookie() {
 
-let name = cname + "=";
+let cname = prompt("Introduzca el nombre de la cookie a leer")
+let v = cname + "=";
 let ca = document.cookie.split(";"); //Divide la cookie separando por ;
     
     for (let i = 0; i < ca.length; i++) { //Recorre cada trozo de la cookie
@@ -21,36 +37,59 @@ let ca = document.cookie.split(";"); //Divide la cookie separando por ;
         c = c.substring(1);
         }
 
-    if (c.indexOf(name) == 0 ) //Si encuentra el campo buscado
-    return c.substring(name.length, c.length) //lo devuelve
+    if (c.indexOf(v) == 0 ) //Si encuentra el campo buscado
+    return c.substring(v.length, c.length) //lo devuelve
     }
 
     return ""; //Si no devuelve vacío
+
 }
 
 //Comprobar si la cookie está inicializada
 
 function checkCookie() {
-    let username = getCookie("username"); //Extrae el valor de la función anterior
+
+let v = getCookie(); //Extrae el value de la función anterior
     
-    if (username != "") {
-        alert("Bienvenido de nuevo" + username) //Lo saludamos
-    } else if (username != "" && username != null) {
-        setCookie() //Se inicializa la cookie
-    } 
+    if (v != "") {
+        alert("Valor de la cookie: " + v) //Nos muestra el valor de la cookie con el nombre que hemos introducido
+    } else if (v != "" && v != null) { //si no existe esa cookie llama al metodo de creacion 
+        setCookie() 
+    }
+
 }
 
-//Actualizar la cookie
+//Actualizar la cookie, simplemente se llama desde dentro a la funcion de creacion
 
 function updateCookie() {
-    setCookie();
+    
+setCookie()
+
 }
 
 //Eliminar la cookie
 
-function deleteCookie(cname) {
-    setCookie(cname, "", -1)
-} 
+function deleteCookie() {
+
+let n = prompt("Introduzca el nombre de la cookie a borrar")
+let v = prompt("Introduzca el valor de la cookie a borrar")
+let d = new Date();
+
+d.setTime(d.getTime() - 1 * 24 * 60 * 60 * 1000);
+let exp = "expires=" + d.toUTCString(); 
+
+document.cookie = n + "=" + v + ";" + exp;
+
+
+}
+
+//Ver las cookies actuales 
+
+function cookies() {
+
+alert("Estas son las cookies actuales: \n" + document.cookie)
+
+}
 
 
 
