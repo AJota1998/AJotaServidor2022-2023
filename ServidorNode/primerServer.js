718958ad1294fc6ecc047e029d3fadb4af336c2b
ajota1998 <alum.aortizg.2021@iesalixar.org>
2022-11-30 12:18:43 +0100

    ej express

--- ServidorNode/primerServer.js
+++ ServidorNode/primerServer.js
@@ -1,10 +1,17 @@
-let http = require('http'); 
+// Importamos el módulo http
+let http = require('http');
 
+// Creamos un servidor con una función con dos parámetros, req la petición y res la respuesta
 const server = http.createServer(function(req, res) {
-    
+
+    // Escribimos la cabecera de  la petición con el código estado 200,
+    // página satisfactoria y el tipo de contenido 'text/plain'    
     res.writeHead(200), {'content.type': 'text/plain'}
+
+    // enviamos 'Hola mundo'
     res.end('Hola mundo');
     });
     
+    // Puerto por el que se escucha
     server.listen(3000)
     console.log('Server listening on http://localhost 3000');
